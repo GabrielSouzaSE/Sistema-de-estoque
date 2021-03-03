6a6d8628a46c4bf7e820406c781d3eb2905f50b7
GabrielSouzaSE <gabrielsouzase@gmail.com>
2021-03-03 01:04:57 -0300

    Create Atendente.java

---
+++ src/estoque/Atendente.java
@@ -0,0 +1,23 @@
+package estoque;
+
+import javax.swing.JOptionPane;
+
+public class Atendente extends Funcionario{
+
+    private int registro;
+
+    public void cadastrarAtendente(){
+
+        super.cadastrarFuncionario();
+        this.registro = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de registro de reclamações"));
+
+    }
+
+    public void exibirAtendente(){
+
+        super.exibirFuncionario();
+        JOptionPane.showMessageDialog(null,
+                "Quant. de registro de reclamações: " + this.registro);
+    }
+
+}
