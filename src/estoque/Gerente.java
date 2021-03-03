31b7d3531374d75b9e4c65830b70b5d7b08ba15b
GabrielSouzaSE <gabrielsouzase@gmail.com>
2021-03-03 01:05:34 -0300

    Create Gerente.java

---
+++ src/estoque/Gerente.java
@@ -0,0 +1,23 @@
+package estoque;
+
+import javax.swing.JOptionPane;
+
+public class Gerente extends Funcionario{
+
+    private double adicionalCargo;
+
+    public void cadastrarGerente(){
+
+        super.cadastrarFuncionario();
+        this.adicionalCargo = Double.parseDouble(JOptionPane.showInputDialog("O valor adicional do Cargo de Gerente"));
+
+    }
+
+    public void exibirGerente(){
+
+        super.exibirFuncionario();
+        JOptionPane.showMessageDialog(null,
+                "Valor adicional pelo cargo de Gerente: " + this.adicionalCargo);
+    }
+
+}
